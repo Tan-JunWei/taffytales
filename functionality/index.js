7940d9bee8a23b1293bf2340e1f66b88cc02f218
jwtan <jwtan0402@gmail.com>
2024-01-18 12:12:43 +0800

    hamburger menu

--- functionality/index.js
+++ functionality/index.js
@@ -1,6 +1,26 @@
-const hamburger = document.querySelector('#hamburger_nav');
-const sidebar = document.querySelector('#side');
+const navLinks = document.querySelector(".nav__links");
+const navItems = document.querySelectorAll(".navItems");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const hbgIcon = document.querySelector(".hbgIcon");
 
-hamburger_nav.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-});
+function toggleMenu() {
+  if (navLinks.classList.contains("showMenu")) {
+    navLinks.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    hbgIcon.style.display = "block";
+  } 
+  else {
+    navLinks.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    hbgIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+navItems.forEach( 
+  function(navItems) { 
+    navItems.addEventListener("click", toggleMenu);
+  }
+)
