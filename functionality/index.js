406b5a69c68cacf19bd5a580e2a75d3d0269a2e6
jwtan <jwtan0402@gmail.com>
2024-01-18 13:47:18 +0800

    closeIcon hidden_debug

--- functionality/index.js
+++ functionality/index.js
@@ -6,16 +6,21 @@ const hbgIcon = document.querySelector(".hbgIcon");
 
 function toggleMenu() {
   if (navLinks.classList.contains("showMenu")) {
+    //close menu
     navLinks.classList.remove("showMenu");
     closeIcon.style.display = "none";
     hbgIcon.style.display = "block";
   } 
   else {
+    //open menu
     navLinks.classList.add("showMenu");
     closeIcon.style.display = "block";
     hbgIcon.style.display = "none";
   }
 }
+
+//button is hidden initially
+closeIcon.style.display = "none";
 
 hamburger.addEventListener("click", toggleMenu);
 
