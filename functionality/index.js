7232cd1e109239a1ed5f9cef763cda1b70b59f6d
jwtan <jwtan0402@gmail.com>
2024-01-22 15:42:25 +0800

    btn ripple effect

--- functionality/index.js
+++ functionality/index.js
@@ -64,3 +64,14 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+
+//button script
+
+const btn = document.querySelector('.btn');
+btn.onmousemove = function(e){
+  const x = e.pageX - btn.offsetLeft;
+  const y = e.pageY - btn.offsetTop;
+
+  btn.style.setProperty('--x', x + 'px');
+  btn.style.setProperty('--y', y + 'px');
+}
