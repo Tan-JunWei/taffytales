89cde79dca3bfb99dcf1e69594c40c104627b7f8
jwtan <jwtan0402@gmail.com>
2024-01-22 11:21:37 +0800

    product pics adjustment + scroll transition

--- functionality/index.js
+++ functionality/index.js
@@ -48,3 +48,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
   autoScroll();
 });
+
+//hidden, scroll effect
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+          entry.target.classList.add('show');
+        }
+        else{
+          entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
