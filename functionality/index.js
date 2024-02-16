be7dfc45dd46195c6be0ce78b2fdf96d795546e8
jwtan <jwtan0402@gmail.com>
2024-02-16 14:23:38 +0800

    nav.html + payment options, copyright

--- functionality/index.js
+++ functionality/index.js
@@ -1,35 +1,3 @@
-//hamburger menu
-const navLinks = document.querySelector(".nav__links");
-const navItems = document.querySelectorAll(".navItems");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const hbgIcon = document.querySelector(".hbgIcon");
-
-function toggleMenu() {
-  if (navLinks.classList.contains("showMenu")) {
-    //close menu
-    navLinks.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    hbgIcon.style.display = "block";
-  } 
-  else {
-    //open menu
-    navLinks.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    hbgIcon.style.display = "none";
-  }
-}
-
-//button is hidden initially
-closeIcon.style.display = "none";
-
-hamburger.addEventListener("click", toggleMenu);
-
-navItems.forEach( 
-  function(navItems) { 
-    navItems.addEventListener("click", toggleMenu);
-  }
-)
 
 //image slider autoscroll effect
 document.addEventListener('DOMContentLoaded', function () {
