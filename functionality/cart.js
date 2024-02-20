e0a41f47f6285baa5387b130a09889e07e5403fa
jwtan <jwtan0402@gmail.com>
2024-02-20 19:44:55 +0800

    add to cart draft1

--- functionality/cart.js
+++ functionality/cart.js
@@ -1,81 +1,81 @@
-const products = [
-  {
-    id: 0,
-    name: "FluffyCloud Bites",
-    price: 11.25,
-    instock: 7,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc:"../assets/marshmallowProduct.png",
-    imgid:"img1",
-  },
-  {
-    id: 1,
-    name: "DivineCocoa Euphoria",
-    price: 10.00,
-    instock: 7,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc: "../assets/chocolateProduct.png",
-    imgid:"img2",
-  },
-  {
-    id: 2,
-    name: "HeavenlyHues Macarons",
-    price: 15.20,
-    instock: 4,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc: "../assets/macaronProduct.png",
-    imgid:"img3",
-  },
-  {
-    id: 3,
-    name: "Minty Velvet Fudge",
-    price: 15.00,
-    instock: 5,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc: "../assets/redVelvetProduct.png",
-    imgid:"img4",
-  },
-  {
-    id: 4,
-    name: "White Chocolate Whisper",
-    price: 16.50,
-    instock: 4,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc: "../assets/whiteChocProduct.png",
-    imgid:"img5",
-  },
-  {
-    id: 5,
-    name: "Blueberry Bliss Buttons",
-    price: 17.90,
-    instock: 8,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-    imgSrc: "../assets/blueberryProduct.png",
-    imgid:"img6",
-  },
-  {
-      id: 6,
-      name: "Pistachio Dream Drops",
-      price: 16.70,
-      instock: 8,
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores",
-      imgSrc: "../assets/pistachioProduct.png",
-      imgid:"img7",
-    },
-];
+let productsInCart = [];
+const parentElement = document.querySelector("#buyItems");
+// const cartSumPrice = document.querySelector(".subtotal"); 
+const products = document.querySelectorAll(".product");
 
-function addToCart(id){
-  products.forEach( (product) => {
-    console.log(id);
+//const countTheSumPrice = function(){
+//  let sumPrice = 0;
+//  productsInCart.forEach(product =>{
+//      sumPrice += product.price;
+//  });
+//}
+
+const updateShoppingCartHTML = function(){
+  if (productsInCart.length >0){
+    let result = productsInCart.map(product =>{ //inner HTML does not change
+      return `
+      <li class="buyItem">
+        <img src="${product.image}">
+        <div>
+          <h5>${product.name}</h5>
+          <h6>$${product.price}</h6>
+          <div>
+            <button class="button-minus" data-id=${product.id}>-</button>  
+            <span class="countOfProduct">${product.count}</span>
+            <button class="button-plus" data-id=${product.id}>+</button>
+          </div>
+        </div>
+      </li>`
   });
+    parent.innerHTML =result.join('');
+    cartSumPrice.innerHTML = "$" + countTheSumPrice(); 
+    console.log("kmy") //works
+  }
+  else{
+    parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>'
+    console.log("PLease")
+    cartSumPrice.innerHTML = ""; 
+  }
+  console.log("txt");
+
 }
+
+function updateProductsInCart(product){ //works
+  for(let i=0; i<productsInCart.length; i++){
+    if(productsInCart[i].id == product.id){
+      productsInCart[i].count +=1;
+      productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+      return;
+    }
+  }
+  productsInCart.push(product);
+  console.log("test"); //works
+}
+
+products.forEach(product => {
+  product.addEventListener("click", (event)=>{ //works
+    if (event.target.classList.contains("btn-cart")){
+      const productID = event.target.dataset.productId;
+      const productName = product.querySelector(".product-name").innerHTML;
+      const productPrice = product.querySelector(".priceValue").innerHTML;
+      const productImage = product.querySelector("img").src;
+
+      let productToCart ={
+        name: productName,
+        image: productImage,
+        id: productID,
+        count: 1,
+        price: +productPrice,
+        basePrice: +productPrice
+      }
+      console.log("Hello World"); //works
+      console.log(productsInCart); //works
+      updateProductsInCart(productToCart);
+      updateShoppingCartHTML();
+    }
+  })
+})
+
 
 //cart slides in from the right
 function hideShowCart() {  
