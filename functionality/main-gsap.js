0428d528cd0a54ba11e09d43aeb6961c7b891c16
jwtan <jwtan0402@gmail.com>
2024-02-27 11:57:21 +0800

    faq section complete

--- functionality/main-gsap.js
+++ functionality/main-gsap.js
@@ -71,3 +71,56 @@ lineTl
     },
     "-=2"
   );
+
+//faq container
+const container = document.querySelector(".faq-container");
+const sections = gsap.utils.toArray(".faq-container section");
+const texts = gsap.utils.toArray(".anim");
+const mask = document.querySelector(".mask");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".faq-container",
+    pin: true,
+    scrub: 1,
+    end: "+=5000",
+    //snap: 1 / (sections.length - 1),
+    //markers: true,
+  }
+});
+
+//Progress bar animation
+
+gsap.to(mask, {
+  width: "100%",
+  scrollTrigger: {
+    trigger: ".wrapper",
+    start: "top left",
+    scrub: 1,
+    //markers:true,
+  }
+});
+
+sections.forEach((section) => {
+  let text = section.querySelectorAll(".anim");
+  
+  // bump out if there's no items to animate
+  if(text.length === 0)  return 
+  
+  //stagger
+  gsap.from(text, {
+    y: -130,
+    opacity: 0,
+    duration: 2,
+    ease: "elastic",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      start: "left center",
+      //markers: true
+    }
+  });
+});
